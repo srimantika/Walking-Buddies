dc6c12eba1c85255428ca731f2fa4a89b48fc9a5
Tania Tasmin <taniat.kaau@email.com>
2022-01-15 11:27:47 -0500

    Create backend and frontend connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import useApplicationData from './hooks/useApplicationData.js'
+
 
 const App = () => {
   const {
       state,
       dispatch
   } = useApplicationData();
-    const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
+    const userList = state.users.map((user) => (<li key={user.id} > {user.name}  {user.email} </li>
 ));
 return (<div className="App" >
   <h1> Users </h1>
