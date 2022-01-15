611205501502efc6466fbd97cf2d6db776e1927a
srimantika <srimantika@gmail.com>
2022-01-15 18:20:07 -0500

    Create Routes for frontend and images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
 import './App.css';
 import useApplicationData from './hooks/useApplicationData.js'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { Switch } from "react-router-dom";
+
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+import User from './components/User'
+import UserList from './components/UserList'
+import Chat from './components/Chat'
 
 
 const App = () => {
@@ -9,11 +23,41 @@ const App = () => {
   } = useApplicationData();
     const userList = state.users.map((user) => (<li key={user.id} > {user.name}  {user.email} </li>
 ));
-return (<div className="App" >
-  <h1> Users </h1>
 
-  <ul> {userList} </ul>
+//<h1> Users </h1>
+//<ul> {userList} </ul>
+
+return (
+
+<Router>
+  <div className="App" >
+
+  <Switch>
+          <Route path="/" exact>
+            <Home />
+
+          </Route>
+          <Route path="/login">
+            <Login />
+
+          </Route>
+          <Route path="/register">
+            <Register />
+
+          </Route>
+
+          <Route path="/UserList">
+            <UserList />  
+        
+          </Route>
+
+          <Route path="/Chat">
+            <Chat />
+            </Route> 
+
+        </Switch>
 </div >
+</Router>
 );
 };
 
---
+++ frontend/src/components/Chat.jsx
@@ -0,0 +1,8 @@
+export default function Chat(){
+  
+  return (
+    <div>
+      Chat Page
+    </div>
+  )
+}
