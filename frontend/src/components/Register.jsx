09366aedb9b62fc6643c25eecfb2aff71a537e8d
Tania Tasmin <taniat.kaau@email.com>
2022-01-16 09:56:11 -0500

    Add more elements and styles in registration form

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -5,12 +5,18 @@ export default function Register(){
   
   const ages = ["20-40","40-60", "60-80"];
   const genders = ["Male","Female"];
+  const reasons = ["Leisure", "Socializing", "Weight Loss", "Staying Fit",  "Walking my pet", "Taking my baby out on a stroll"];
+  const times = ["Morning","Afternoon","Evening"];
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
+  const [location, setLocation] = useState("");
+  const [areacode, setAreacode] = useState("");
+  const [reason, setReason] = useState("");
+  const [time, setTime] = useState("");
 
 
   const handleSubmit = (e) =>{
@@ -19,6 +25,10 @@ export default function Register(){
     console.log(password);
     console.log(age);
     console.log(gender);
+    console.log(location);
+    console.log(areacode);
+    console.log(reason);
+    console.log(time);
   }
   
   return (
@@ -28,19 +38,19 @@ export default function Register(){
         <form autoComplete="off"  onSubmit={event => event.preventDefault()}>
             <div className="form__group">
               <label>Name: </label>
-              <input type="text" value={name} placeholder="Enter Name" 
+              <input type="text" value={name} placeholder="Name" 
                 onChange={(event) => setName(event.target.value)}
               />
             </div>
             <div className="form__group">
               <label>Email:</label>
-              <input type="email" value={email} placeholder="Enter Email"      
+              <input type="email" value={email} placeholder="Email"      
                 onChange={(event) => setEmail(event.target.value)}
               />
             </div>
             <div className="form__group">
               <label>Password:</label>
-              <input type="password" value={password} placeholder="Enter Password "      
+              <input type="password" value={password} placeholder="Password "      
                 onChange={(event) => setPassword(event.target.value)}
               />
             </div>
@@ -56,6 +66,34 @@ export default function Register(){
                 {genders.map(gender => (<option key={gender}>{gender}</option>))}
               </select>  
             </div>
+            <div className="form__group">
+              <label>Location:</label>
+              <input type="text" value={location} placeholder="Address"      
+                onChange={(event) => setLocation(event.target.value)}
+              />
+            </div>
+            <div className="form__group">
+              <label>Area:</label>
+              <input type="text" value={areacode} placeholder="Area Code"      
+                onChange={(event) => setAreacode(event.target.value)}
+              />
+            </div>
+            <label className="form__group">Preferences</label>
+            < div className="form__group2">
+              <div className="form__group">
+                <label>Reason for Walk:</label>
+                <select value={reason} onChange={(event) => setReason(event.target.value)}>
+                  {reasons.map(reason => (<option key={reason}>{reason}</option>))}
+                </select>  
+              </div>
+              <div className="form__group">
+                <label>Time of Walk:</label>
+                <select value={time} onChange={(event) => setTime(event.target.value)}>
+                  {times.map(time => (<option key={time}>{time}</option>))}
+                </select>  
+              </div>
+            </div>
+            
         </form>
         <button type="button" className="btn" onClick={handleSubmit}>
             Register
