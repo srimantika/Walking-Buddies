c88a1be007e863f1230ce78cad950f0c3841c32a
Mohamed Shahul Hameed <thameemsh@email.com>
2022-01-17 07:11:30 -0500

    Create user information card

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,8 +1,41 @@
+import React, { Fragment } from 'react'
+
+import '../styles/variables.scss'
+import './UserList.scss'
+import User from './User'
+
+import bgimage from '../images/backgroundimg3.jpg'
+
+import "./User.scss";
+
 export default function UserList(){
-  
+
   return (
-    <div>
-      UserList
-    </div>
+    <Fragment>
+<img 
+ src={bgimage}
+ className='top-image'
+/>
+<div className='filter-section'>
+  <button className='btn'>Filter</button>
+  <div className='options-section'>
+    <button>
+      Age
+    </button>
+    <button>
+     Gender
+    </button>
+    <button>
+      Time of Walk
+    </button>
+    <button>
+      Reason of Walk
+    </button>
+  </div>
+</div>
+
+<User />
+    </Fragment>
+
   )
 }
