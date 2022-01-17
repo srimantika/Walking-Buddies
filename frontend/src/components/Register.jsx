c28c41822f42da3da400ffa7954d40f66048fadd
Tania Tasmin <taniat.kaau@email.com>
2022-01-17 14:51:24 -0500

    Fix the alignment problems in the registration page

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -10,10 +10,12 @@ export default function Register(){
   const genders = ["Male","Female"];
   const reasons = ["Leisure", "Socializing", "Weight Loss", "Staying Fit",  "Walking my pet", "Taking my baby out on a stroll"];
   const times = ["Morning","Afternoon","Evening"];
-  const interests= ["Games", "Music", "Movies", "Reading","Fitness and Wellness",
-                    "Arts and Music", "Home and Garden","Politics and Social issues",
-                    "Travel", "Shopping and Fashion", "Sports and Outdoors",
-                    "Technology", "Automobiles","Food and Drinks"];
+  const interests= ["Games 🎮", "Music 🎵", "Movies 🎬", "Reading 📚","Sports ⚽",
+                    "Food & Drinks ☕", "Arts 🎨","Politics 🤵",
+                    "Travel 🌎", "Fashion 👗", "Fitness 🏋️‍♂️",
+                    "Technology 💻 ", "Automobiles 🚗","Gardening 🌳"];
+
+                    
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -25,16 +27,24 @@ export default function Register(){
   const [reason, setReason] = useState("");
   const [time, setTime] = useState("");
   const interest=[];
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState( 
+    {profileImg:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'});
 
   const [checkedItems, setCheckedItems] = useState({}); //plain object as state
 
   const handleChange = (event) => {
-      // updating an object instead of a Map
-      
-      setCheckedItems({...checkedItems, [event.target.name] : event.target.checked });
-      console.log(checkedItems);
-    }
+     setCheckedItems({...checkedItems, [event.target.name] : event.target.checked });
+  }
+
+    const imageHandler = (e) => {
+      const reader = new FileReader();
+      reader.onload = () =>{
+        if(reader.readyState === 2){
+          setSelectedImage({profileImg: reader.result})
+        }
+      }
+      reader.readAsDataURL(e.target.files[0])
+    };
 
   const handleSubmit = (e) =>{
     console.log(name);
@@ -46,7 +56,7 @@ export default function Register(){
     console.log(areacode);
     console.log(reason);
     console.log(time);
-    console.log(selectedImage.name);
+    console.log(selectedImage.profileImg);
 
     for (let key in checkedItems ){
       if (checkedItems[key]===true){
@@ -59,52 +69,52 @@ export default function Register(){
 
   }
 
+  const { profileImg}=selectedImage;
   
   return (
     <div className="container_register">
-      Register
-      <div className="form">
+         
         <form autoComplete="off"  onSubmit={event => event.preventDefault()}>
             
-            <div className="form__group1">
+            <div className="group1">
             <div className="main_profile">
-              <div className="form__group">
+              <div className="group">
                 <label>Name: </label>
                 <input type="text" value={name} placeholder="Name" 
                   onChange={(event) => setName(event.target.value)}
                 />
               </div>
-              <div className="form__group">
+              <div className="group">
                 <label>Email:</label>
                 <input type="email" value={email} placeholder="Email"      
                   onChange={(event) => setEmail(event.target.value)}
                 />
               </div>
-              <div className="form__group">
+              <div className="group">
                 <label>Password:</label>
                 <input type="password" value={password} placeholder="Password "      
                   onChange={(event) => setPassword(event.target.value)}
                 />
               </div>
-              <div className="form__group">
+              <div className="group">
                 <label>Age:</label>
                 <select value={age} onChange={(event) => setAge(event.target.value)}>
                   {ages.map(age => (<option key={age}>{age}</option>))}
                 </select>  
               </div>
-              <div className="form__group">
+              <div className="group">
                 <label>Gender:</label>
                 <select value={gender} onChange={(event) => setGender(event.target.value)}>
                   {genders.map(gender => (<option key={gender}>{gender}</option>))}
                 </select>  
               </div>
-              <div className="form__group">
+              <div className="group">
                 <label>Location:</label>
                 <input type="text" value={location} placeholder="Address"      
                   onChange={(event) => setLocation(event.target.value)}
                 />
               </div>
-              <div className="form__group">
+              <div className="group">
                 <label>Area:</label>
                 <input type="text" value={areacode} placeholder="Area Code"      
                   onChange={(event) => setAreacode(event.target.value)}
@@ -112,43 +122,34 @@ export default function Register(){
               </div>
             </div>
 
-            <div className="picture">
+            
               
-              {selectedImage && (
-               <div>
-               <img alt="not found" resizeMode="contain"style={{
-                  flex: 1,
-                  alignSelf: 'stretch',
-                  width: undefined,
-                  height: undefined
-                }} src={URL.createObjectURL(selectedImage)} />
-               <br />
-               <button onClick={()=>setSelectedImage(null)}>Remove</button>
-              </div>
-               )}
-              <br />
-     
-               <br /> 
-              <input
-               type="file"
-               id = "file"
-               name="myImage"
-                onChange={(event) => {
-                setSelectedImage(event.target.files[0]);
-                }}
-              />
-            </div>
-            </div>
+            
+				<div className="image-container">
+					{/* <h3 className="image-heading">Add your Image</h3> */}
+					<div className="img-holder">
+						<img src={profileImg} alt="" id="img" className="img" />
+					</div>
+					<input type="file" accept="image/*" name="image-upload" id="image-input" onChange={imageHandler} />
+					<div className="image-label">
+          <label className="image-upload" htmlFor="image-input">
+						{/* <i className="material-icons">add_photo_alternate</i> */}
+						Choose your Photo
+					</label>
+          </div>
+			  </div>
+      </div>
 
-            <label className="form__group">Preferences</label>
-            < div className="form__group2">
-              <div className="form__group">
+      <label className="preference"> Preferences</label>
+            < div className="group2">
+        
+              <div className="space2">
                 <label>Reason for Walk:</label>
                 <select value={reason} onChange={(event) => setReason(event.target.value)}>
                   {reasons.map(reason => (<option key={reason}>{reason}</option>))}
                 </select>  
               </div>
-              <div className="form__group">
+              <div className="space2">
                 <label>Time of Walk:</label>
                 <select value={time} onChange={(event) => setTime(event.target.value)}>
                   {times.map(time => (<option key={time}>{time}</option>))}
@@ -156,20 +157,22 @@ export default function Register(){
               </div>
             </div>
 
-          <div className="form__group3">
-          <label>Interest </label> <br/>
-          {
+         <label className="preference">Interest </label>
+          <div className="group3">
+               {
               interests.map(item => (
                   <label key={item}>
                       <Checkbox name={item} checked={checkedItems[item]} onChange={handleChange} />
-                      {item}
+                      <span>{item}</span>
                   </label>
               ))
           }
           </div>
           
+         
+          
         </form>
-      </div>
+      
       <button type="button" className="btn" onClick={handleSubmit}>
         Register
       </button>
