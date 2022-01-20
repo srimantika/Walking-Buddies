134bfbf881396b23e2910b5fe6a4391c1fa53672
Tania Tasmin <taniat.kaau@email.com>
2022-01-20 09:50:49 -0500

    Add seed data for users

--- backend/db/seeds/users.sql
+++ backend/db/seeds/users.sql
@@ -1,8 +1,38 @@
-
-
+INSERT INTO
+users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
+VALUES
+    ('Mario Alata','https://i.imgur.com/FK8V841.jpg', 'mario@nintendo.com', 'test', '25-35', 'male', '5333 - Glen Erin Drive', 
+    'Mississauga', 'L5B 0B2', 'Leisure', 'Morning', 'Games 🎮, Travel 🌎,Automobiles 🚗"');
 
 
 INSERT INTO
 users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
 VALUES
-    ('Mario','https://i.imgur.com/FK8V841.jpg', 'mario@nintendo.com', 'test', '25-35', 'male', 'Glen Erin Drive', 'Mississauga', 'L5B 4M8', 'Leisure', 'Morning', 'Gaming, Travel');
+    ('Katlin Rossa','https://i.imgur.com/LpaY82x.png', 'Katlin@rossa.com', 'test', '35-45', 'Female', '9812 - Thomas Street', 'Mississauga', 
+    'L5M 1Y1', 'Leisure', 'Morning', 'Reading 📚, Gardening 🌳');
+
+
+INSERT INTO
+users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
+VALUES
+    ('Joey MacDonald','https://i.imgur.com/Nmx0Qxo.png', 'joey@macdonald.com', 'test', '35-45', 'Male', '5665 - Glen Erin Drive', 'Mississauga', 
+    'L5M 0A3', 'Leisure', 'Morning', 'Arts 🎨, Gardening 🌳,Technology 💻 '); 
+
+INSERT INTO
+users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
+VALUES
+    ('Samantha Stanic','https://i.imgur.com/okB9WKC.jpg', 'samantha@stanic.com', 'test', '23-35', 'Female', '4513 - Streetsville', 'Mississauga', 
+    'L5M 0A1', 'Leisure', 'Morning', 'Arts 🎨, Fitness 🏋️‍♂️,Fashion 👗'); 
+
+INSERT INTO
+users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
+VALUES
+    ('Alec Quon','https://i.imgur.com/3tVgsra.jpg', 'alec@quon.com', 'test', '45-55', 'Male', '1234 - Streetsville', 'Mississauga', 
+    'L5M 0B8', 'Leisure', 'Morning', 'Movies 🎬, Gardening 🌳,Fitness 🏋️‍♂️'); 
+
+INSERT INTO
+users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
+VALUES
+    ('Sven Jones','https://i.imgur.com/twYrpay.jpg', 'sven@jones.com', 'test', '45-55', 'Male', '123 - Hurontario Street', 'Mississauga', 
+    'L5R 0B8', 'Leisure', 'Morning', 'Movies 🎬, Gardening 🌳,Fitness 🏋️‍♂️'); 
+
