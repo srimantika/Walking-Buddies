ffc056fc41447c96dbc6902cd81cfedae0a2ca3c
Tania Tasmin <taniat.kaau@email.com>
2022-01-21 13:56:51 -0500

    Create Chat window

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,8 +1,19 @@
+import { ChatEngine } from 'react-chat-engine';
+import './Chat.scss';
+import ChatFeed from './ChatFeed';
+
+
 export default function Chat(){
   
   return (
-    <div>
-      Chat Page
-    </div>
-  )
+    <ChatEngine
+    height="100vh"
+    projectID="955437cd-7fb2-4a87-b788-6f0f9faa6d1b"
+    userName="Mario Nintendo"
+    // userName="Katlin Rossa"
+    userSecret="test"
+    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+  
 }
