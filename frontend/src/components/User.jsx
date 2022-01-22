1d055ee8bba72b521db4db325f294f397cddf56a
Mohamed Shahul Hameed <thameemsh@gmail.com>
2022-01-22 18:39:47 -0500

    Add redirect to open chat in new window

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -4,101 +4,85 @@ import { Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 import "./User.scss";
 
+// import {createDirectChat, renderChatForm} from "./DMhelpers"
+
 export default function User(props) {
-const [chat, setChat] = useState(false)
 
-
-console.log("props from the user.jsx", props)
-  const clickHandler = () => {
-    setChat(true);
-  };
-  
-  
-  const genderImg = 
-  (props.gender === 'Male' || props.gender === 'male') ? '/images/malesingle.png' : '/images/femalesingle.png';
+  const genderImg =
+    props.gender === "Male" || props.gender === "male"
+      ? "/images/malesingle.png"
+      : "/images/femalesingle.png";
 
   return (
     <Fragment>
+      <article>
+        <div className="left-section">
+          <div className="photo-info-container">
+            <div className="photo-container">
+              <img src={props.picture} className="photo" />
+            </div>
+            <div className="right-of-photo">
+              <div className="name-age">
+                <div className="name-icon-container">
+                  <div className="name-icon">
+                    <img src={genderImg} className="icon-name" />
+                    <span className="name">
+                      <strong>{props.name} </strong>
+                    </span>
+                  </div>
 
-  
-
-<article>
-      <div className="left-section">
-        <div className="photo-info-container">
-          <div className='photo-container'>
-            <img
-              src={props.picture}
-              className="photo"
-            />
-          </div>
-          <div className="right-of-photo">
-            <div className="name-age">
-              <div className="name-icon-container">
-                <div className="name-icon">
-                  <img
-                    src={genderImg}
-                    className="icon-name"
-                  />
-                   <span className="name"><strong>{props.name}  </strong></span>
+                  <div className="age-container">
+                    <strong>Age:</strong> {props.age} years
+                  </div>
                 </div>
-
-               
-
-                <div className="age-container"><strong>Age:</strong> {props.age} years</div>
               </div>
 
-              
+              <div className="location">
+                <p>
+                  <strong>Location:</strong> {props.street_name}, {props.city}{" "}
+                </p>
+              </div>
+              <div className="likes">
+                <p>
+                  <strong>Reason for Walk:</strong> {props.walk_reason}
+                </p>
+              </div>
+
+              <div className="interests">
+                <p>
+                  <strong>Interests:</strong> {props.interests}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="right-section">
+          <div className="preferred-time-user">
+            <div>
+              <p>Preferred Time of Walk</p>
+            </div>
+            <div className="clock-image-container">
+              <img
+                src="/images/clock.png"
+                alt="clock-image"
+                className="time-of-day"
+              />
             </div>
 
-            <div className='location'>
-              <p><strong>Location:</strong> {props.street_name}, {props.city} </p>
+            <div>
+              <p id="afternoon-tag">{props.walk_time}</p>
             </div>
-            <div className='likes'>
-          <p><strong>Reason for Walk:</strong> {props.walk_reason}</p>
-      
-        </div>
-
-        <div className='interests'>
-          <p><strong>Interests:</strong> {props.interests}</p>
-        </div>
           </div>
-        </div>
+          <div className="chat-button-section">
 
-        
-      </div>
-
-      <div className="right-section">
-        <div className="preferred-time-user">
-          <div>
-            <p>Preferred Time of Walk</p>
-          </div>
-          <div className="clock-image-container">
-            <img
-              src="/images/clock.png"
-              alt='clock-image'
-              className="time-of-day"
-            />
-          </div>
-
-          <div>
-            <p id='afternoon-tag'>{props.walk_time}</p>
-          </div>
-        </div>
-        <div className="chat-button-section">
-     
-          <button
-          onClick={clickHandler} 
-          className="chatbutton"><span>Message Me!</span>
+          <button type="button" className="chatbutton">
+          <a href="/Chat" target="_blank">Message Me!</a>
           </button>
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
 
-     
-    {chat === true && <Redirect to={{pathname: '/Chat',  state:{name:props.currentUserName, password:props.currentUserPassword}}} />}
-
-    
     </Fragment>
-    
   );
 }
