2747fef0049a0ae195159ee2f9c919c2c100676c
Mohamed Shahul Hameed <thameemsh@gmail.com>
2022-01-21 19:39:03 -0500

    Make chat login details dynamic

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -3,15 +3,15 @@ import './Chat.scss';
 import ChatFeed from './ChatFeed';
 
 
-export default function Chat(){
-  
+export default function Chat(props){
+  console.log("props:",props)
   return (
     <ChatEngine
     height="100vh"
     projectID="955437cd-7fb2-4a87-b788-6f0f9faa6d1b"
-    userName="Mario Nintendo"
+    userName={props.location.state.name}
     // userName="Katlin Rossa"
-    userSecret="test"
+    userSecret={props.location.state.password}
     renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
