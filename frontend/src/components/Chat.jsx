e10f38d9e1bd0905b241ec2c4014330768723444
Mohamed Shahul Hameed <thameemsh@gmail.com>
2022-01-22 18:35:23 -0500

    Revise Chat component to open in new window

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,19 +1,34 @@
-import { ChatEngine } from 'react-chat-engine';
-import './Chat.scss';
-import ChatFeed from './ChatFeed';
+import { useState, useEffect } from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./Chat.scss";
+import ChatFeed from "./ChatFeed";
+
+export default function Chat(props) {
+
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
 
 
-export default function Chat(props){
-  console.log("props:",props)
-  return (
-    <ChatEngine
-    height="100vh"
-    projectID="955437cd-7fb2-4a87-b788-6f0f9faa6d1b"
-    userName={props.location.state.name}
-    // userName="Katlin Rossa"
-    userSecret={props.location.state.password}
-    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-    />
-  );
-  
+  useEffect(() => {
+
+    setName(localStorage.getItem("name"));
+    setPassword(localStorage.getItem("password"));
+
+  }, []);
+
+ 
+   if (name && password) {
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID="955437cd-7fb2-4a87-b788-6f0f9faa6d1b"
+        userName={name}
+        userSecret={password}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
+    );
+  } 
+  else {
+    return <> </>;
+  }
 }
