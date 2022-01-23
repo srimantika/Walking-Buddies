d2e61cbf72ead22783c71ef498c1ac1a5445eac4
srimantika <srimantika@gmail.com>
2022-01-23 14:54:43 -0500

    Add seed data with good resolution pics

--- backend/db/seeds/users.sql
+++ backend/db/seeds/users.sql
@@ -1,38 +1,44 @@
 INSERT INTO
 users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
 VALUES
-    ('Mario Alata','https://i.imgur.com/FK8V841.jpg', 'mario@nintendo.com', 'test', '25-35', 'male', '5333 - Glen Erin Drive', 
-    'Mississauga', 'L5B 0B2', 'Leisure', 'Morning', 'Games 🎮, Travel 🌎,Automobiles 🚗"');
+    ('Mario Alata','https://images.pexels.com/photos/848573/pexels-photo-848573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'mario@nintendo.com', 'test', '25-35', 'Male', '5333 - Glen Erin Drive', 
+    'Mississauga', 'L5M 0B2', 'Socializing', 'Afternoon', 'Games 🎮, Travel 🌎,Automobiles 🚗"');
 
 
 INSERT INTO
 users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
 VALUES
-    ('Katlin Rossa','https://i.imgur.com/LpaY82x.png', 'Katlin@rossa.com', 'test', '35-45', 'Female', '9812 - Thomas Street', 'Mississauga', 
-    'L5M 1Y1', 'Leisure', 'Morning', 'Reading 📚, Gardening 🌳');
+    ('Katlin Rossa','https://images.pexels.com/photos/792725/pexels-photo-792725.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'Katlin@rossa.com', 'test', '35-45', 'Female', '9812 - Thomas Street', 'Mississauga', 
+    'L5M 1Y1', 'Walking my pet', 'Evening', 'Reading 📚, Gardening 🌳');
 
 
 INSERT INTO
 users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
 VALUES
-    ('Joey MacDonald','https://i.imgur.com/Nmx0Qxo.png', 'joey@macdonald.com', 'test', '35-45', 'Male', '5665 - Glen Erin Drive', 'Mississauga', 
+    ('Joey MacDonald','https://images.pexels.com/photos/694587/pexels-photo-694587.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'joey@macdonald.com', 'test', '35-45', 'Male', '5665 - Glen Erin Drive', 'Mississauga', 
     'L5M 0A3', 'Leisure', 'Morning', 'Arts 🎨, Gardening 🌳,Technology 💻 '); 
 
 INSERT INTO
 users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
 VALUES
-    ('Samantha Stanic','https://i.imgur.com/okB9WKC.jpg', 'samantha@stanic.com', 'test', '23-35', 'Female', '4513 - Streetsville', 'Mississauga', 
-    'L5M 0A1', 'Leisure', 'Morning', 'Arts 🎨, Fitness 🏋️‍♂️,Fashion 👗'); 
+    ('Samantha Stanic','https://images.pexels.com/photos/2690323/pexels-photo-2690323.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'samantha@stanic.com', 'test', '23-35', 'Female', '4513 - Streetsville', 'Mississauga', 
+    'L5M 0A1', 'Socializing', 'Afternoon', 'Arts 🎨, Fitness 🏋️‍♂️,Fashion 👗'); 
 
 INSERT INTO
 users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
 VALUES
-    ('Alec Quon','https://i.imgur.com/3tVgsra.jpg', 'alec@quon.com', 'test', '45-55', 'Male', '1234 - Streetsville', 'Mississauga', 
+    ('Alec Quon','https://images.pexels.com/photos/1270076/pexels-photo-1270076.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'alec@quon.com', 'test', '45-55', 'Male', '1234 - Streetsville', 'Mississauga', 
+    'L5M 0B8', 'Walking my pet', 'Evening', 'Movies 🎬, Gardening 🌳,Fitness 🏋️‍♂️'); 
+
+INSERT INTO
+users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
+VALUES
+    ('Sven Jones','https://images.pexels.com/photos/737586/pexels-photo-737586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'sven@jones.com', 'test', '45-55', 'Male', '123 - Hurontario Street', 'Mississauga', 
     'L5M 0B8', 'Leisure', 'Morning', 'Movies 🎬, Gardening 🌳,Fitness 🏋️‍♂️'); 
 
 INSERT INTO
 users(name, picture, email, password, age, gender, street_name, city, postal_code, walk_reason, walk_time, interests)
 VALUES
-    ('Sven Jones','https://i.imgur.com/twYrpay.jpg', 'sven@jones.com', 'test', '45-55', 'Male', '123 - Hurontario Street', 'Mississauga', 
-    'L5R 0B8', 'Leisure', 'Morning', 'Movies 🎬, Gardening 🌳,Fitness 🏋️‍♂️'); 
+    ('Amanda Baretto','https://images.pexels.com/photos/3894557/pexels-photo-3894557.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'amanda@baretto.com', 'test', '35-45', 'Female', 'Thomas Street', 'Mississauga', 
+    'L5M 6Y2', 'Walking my pet', 'Evening', 'Reading 📚, Gardening 🌳,Fitness 🏋️‍♂️');    
 
